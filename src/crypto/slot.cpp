1e5001233d3a4a59c6d30d4a304a4164a6144eb4
Chris Johnson <chrisjohnsonmail@gmail.com>
2019-11-06 12:09:52 -0600

    feat: add initial support for the cortex m4

--- src/crypto/slot.cpp
+++ src/crypto/slot.cpp
@@ -53,9 +53,7 @@
   }  // namespace Crypto
   }  // namespace Ark
 
-#endif
-
-#if defined(ESP32) || defined(ESP8266)
+#else
 
   #include <ctime>
   #include <stdlib.h> /* strtol */
@@ -78,7 +76,7 @@
     const uint16_t YEAR_1900     = 1900U;
 
     //  If unexpected ISO 8601 date/time length
-    if (network.epoch.length() != TIMESTAMP_LEN) {  
+    if (network.epoch.length() != TIMESTAMP_LEN) {
       return 0ULL;
     };
 
